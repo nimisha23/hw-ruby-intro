64dcf9ae10462ba7494245558ef8d41c31436989
EC2 Default User <ec2-user@ip-172-31-14-212.ec2.internal>
2022-02-11 23:50:21 +0000

    Completed Programming Assignment 1: Ruby Intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,61 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).each {|c| return true if (c[0] + c[1] == n)}
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.empty?
+    return false
+  else
+    s=s.downcase
+    if /^[^aeiou\W]/ =~ s[0]
+      return true
+    else
+      return false
+    end
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s =~ /^[0-1]+$/) && (s.to_i % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    @isbn=isbn
+    @price=price
+
+    if @isbn.empty? || @price <= 0 
+      raise ArgumentError
+    end
+  end
+	
+  attr_accessor :isbn
+  attr_accessor :price
+
+  def price_as_string
+    "$#{'%.2f' %  @price}"
+  end
 end
